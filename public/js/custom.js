542c736f02da0fc2598e05ba610af91e816821ea
S.Sairam <1414sairam@gmail.com>
2022-04-15 09:57:49 +0530

    Rework

--- public/js/custom.js
+++ public/js/custom.js
@@ -158,7 +158,7 @@ $(document).ready(function () {
   setTimeout(function () {
     $('body').addClass('loaded');
     // $('h1').css('color', '#222222');
-  }, 3000);
+  }, 1300);
 
 });
 $(function () {
